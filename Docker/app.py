63c41e0a289685c5380866a617f30693e64b79f6
ogzyzy <157073744+ogzyzy@users.noreply.github.com>
2024-03-25 23:32:12 +0100

    Update app.py

--- Docker/app.py
+++ Docker/app.py
@@ -4,40 +4,39 @@ from PIL import Image
 import io
 
 s3_client = boto3.client('s3')
-dest_bucket_name = 'image-scaler-destinationbucket'  #Change the destination bucket name
+dest_bucket_name = 'destination_bucket_name'  # Zmień na nazwę bucketu docelowego
 
 def lambda_handler(event, context):
     try:
-        # Retrieving information about the uploaded image
+        # Pobieranie informacji o przesłanym obrazie
         s3_event = event['Records'][0]['s3']
         bucket_name = s3_event['bucket']['name']
         object_key = s3_event['object']['key']
-
-        # Downloading an image from an S3 bucket
+        
+        # Pobieranie obrazu z bucketu S3
         response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
         object_content = response['Body'].read()
 
-        # Opening an image using Pillow
+        # Otwieranie obrazu przy użyciu Pillow
         image = Image.open(io.BytesIO(object_content))
-
-        # Scaling the image by half
-        # Change the scale as you need
+        
+        # Skalowanie obrazu o połowę
         new_size = (int(image.width / 2), int(image.height / 2))
         resized_image = image.resize(new_size)
-
-        # Preparing the scaled image for saving
+        
+        # Przygotowanie przeskalowanego obrazu do zapisu
         buffer = io.BytesIO()
         resized_image.save(buffer, format=image.format)
         buffer.seek(0)
-
-        # Saving the scaled image in the destination bucket
-        s3_client.put_object(Body=buffer, Bucket=dest_bucket_name, Key=f'resize$
+        
+        # Zapisanie przeskalowanego obrazu w bucketu docelowym
+        s3_client.put_object(Body=buffer, Bucket=dest_bucket_name, Key=f'resized/{object_key}')
 
         return {
             'statusCode': 200,
             'body': json.dumps('Successfully scaled image')
         }
-
+        
     except Exception as e:
         print(f'Error: {e}')
         return {
