7e3ca97521bdca221ac99e86a985cf3d2ca315ed
ogzyzy <157073744+ogzyzy@users.noreply.github.com>
2024-03-25 18:11:39 +0100

    Update and rename image-scaler-code.py to app.py
    image scaling added

---
+++ app.py
@@ -0,0 +1,46 @@
+import json
+import boto3
+from PIL import Image
+import io
+
+s3_client = boto3.client('s3')
+dest_bucket_name = 'image-scaler-destinationbucket'  #Change the destination bucket name
+
+def lambda_handler(event, context):
+    try:
+        # Retrieving information about the uploaded image
+        s3_event = event['Records'][0]['s3']
+        bucket_name = s3_event['bucket']['name']
+        object_key = s3_event['object']['key']
+
+        # Downloading an image from an S3 bucket
+        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
+        object_content = response['Body'].read()
+
+        # Opening an image using Pillow
+        image = Image.open(io.BytesIO(object_content))
+
+        # Scaling the image by half
+        # Change the scale as you need
+        new_size = (int(image.width / 2), int(image.height / 2))
+        resized_image = image.resize(new_size)
+
+        # Preparing the scaled image for saving
+        buffer = io.BytesIO()
+        resized_image.save(buffer, format=image.format)
+        buffer.seek(0)
+
+        # Saving the scaled image in the destination bucket
+        s3_client.put_object(Body=buffer, Bucket=dest_bucket_name, Key=f'resize$
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Successfully scaled image')
+        }
+
+    except Exception as e:
+        print(f'Error: {e}')
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error scaling image')
+        }
