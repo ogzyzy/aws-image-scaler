2b93323973c868cc0e60a7abc4fc6792a765ea05
ogzyzy <157073744+ogzyzy@users.noreply.github.com>
2024-04-09 20:23:50 +0200

    Update app.py

--- Docker/app.py
+++ Docker/app.py
@@ -4,32 +4,32 @@ from PIL import Image
 import io
 
 s3_client = boto3.client('s3')
-dest_bucket_name = 'destination_bucket_name'  # Zmień na nazwę bucketu docelowego
+dest_bucket_name = 'destination_bucket_name'  # Change to the name of the destination bucket
 
 def lambda_handler(event, context):
     try:
-        # Pobieranie informacji o przesłanym obrazie
+        # Retrieving information about the uploaded image
         s3_event = event['Records'][0]['s3']
         bucket_name = s3_event['bucket']['name']
         object_key = s3_event['object']['key']
         
-        # Pobieranie obrazu z bucketu S3
+        # Downloading the image from the S3 bucket
         response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
         object_content = response['Body'].read()
 
-        # Otwieranie obrazu przy użyciu Pillow
+        # Opening the image using Pillow
         image = Image.open(io.BytesIO(object_content))
         
-        # Skalowanie obrazu o połowę
+        # Scaling the image by half
         new_size = (int(image.width / 2), int(image.height / 2))
         resized_image = image.resize(new_size)
         
-        # Przygotowanie przeskalowanego obrazu do zapisu
+        # Preparing the scaled image for saving
         buffer = io.BytesIO()
         resized_image.save(buffer, format=image.format)
         buffer.seek(0)
         
-        # Zapisanie przeskalowanego obrazu w bucketu docelowym
+        # Saving the scaled image in the destination bucket
         s3_client.put_object(Body=buffer, Bucket=dest_bucket_name, Key=f'resized/{object_key}')
 
         return {
